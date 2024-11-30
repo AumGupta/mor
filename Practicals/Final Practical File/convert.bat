9421721b7ca9df23585a8736b329c3478ca1c8c5
Om Gupta <aaumgupta@gmail.com>
2024-11-30 16:01:15 +0530

    Finalised Pracical PDF

--- Practicals/Final Practical File/convert.bat
+++ Practicals/Final Practical File/convert.bat
@@ -1,4 +1,4 @@
 cd "Practicals/Final Practical File"
 jupyter nbconvert --to pdf main.ipynb
-pdftk "main.pdf" cat 2-end output "Final Practical File - Om Gupta.pdf"
+pdftk "main.pdf" cat 2-end output "21SGGCBSCS000037(Om Gupta).pdf"
 del main.pdf

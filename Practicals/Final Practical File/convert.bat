39d59c094dcfbd4db35d15f51880e07b0ca59fc4
Om Gupta <aaumgupta@gmail.com>
2024-12-01 02:06:01 +0530

    Final PDF Render

--- Practicals/Final Practical File/convert.bat
+++ Practicals/Final Practical File/convert.bat
@@ -1,4 +1,4 @@
 cd "Practicals/Final Practical File"
 jupyter nbconvert --to pdf main.ipynb
-pdftk "main.pdf" cat 2-end output "21SGGCBSCS000037(Om Gupta).pdf"
+pdftk "main.pdf" cat 2-end output "24236761066(Om Gupta).pdf"
 del main.pdf

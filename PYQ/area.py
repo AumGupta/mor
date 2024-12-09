53119075ea9cf70f1b9a07a2e189d920da88fe47
Om Gupta <aaumgupta@gmail.com>
2024-12-10 00:08:17 +0530

    Added PYQ Solutions

---
+++ PYQ/area.py
@@ -0,0 +1,17 @@
+import math  # Importing math for circle area calculation
+
+# Function to calculate the area of a triangle
+def triangle(base, height):
+    return 0.5 * base * height
+
+# Function to calculate the area of a square
+def square(side):
+    return side ** 2
+
+# Function to calculate the area of a rectangle
+def rectangle(length, width):
+    return length * width
+
+# Function to calculate the area of a circle
+def circle(radius):
+    return math.pi * radius ** 2
